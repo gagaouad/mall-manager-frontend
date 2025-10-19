ffc7b4e029b95f6b6707f18f60f51c8519fc74fe
Djamal GAGAOUA <gagaouad@live.fr>
2025-10-19 14:12:31 +0200

    app basic layout

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
 </template>
---
+++ app/pages/stores.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    store
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
